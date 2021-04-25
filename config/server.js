a848ab3bcf268fced8df4296123c15767626ddb7
Douglas <douglasjrosa@gmail.com>
2021-04-25 12:23:40 -0300

    Next and Strapi install

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ccb485273e14ed050c02bb456eca1b2b'),
+    },
+  },
+});
